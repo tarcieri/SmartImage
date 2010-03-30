c68e4243823bdb66ddf6c4db1c88df7da4778143
Tony Arcieri <tony@medioh.com>
2010-03-30 00:24:33 -0600

    require 'smartimage' should Just Work

---
+++ lib/smartimage.rb
@@ -0,0 +1,15 @@
+# Maybe you want to:
+#
+#   require 'smartimage'
+#
+# Instead of:
+#
+#   require 'smart_image'
+#
+# Yes SmartImage is CamelCase and smart_image is the String#underscore of
+# that, but seriously, shouldn't require 'smartimage' Just Work?
+#
+# Yes, yes it should...
+#
+
+require 'smart_image'
